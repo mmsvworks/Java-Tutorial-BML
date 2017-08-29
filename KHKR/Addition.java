a95ee9b9903ef6bf5bb1a8d2a4f37334dd6bcbfb
Harish Reddy <khkr@MBP.local>
2017-08-29 22:24:34 +0530

    Addition update

---
+++ KHKR/Addition.java
@@ -0,0 +1,19 @@
+public class Addition
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+
+		int c = a+b;
+
+		char f = 'e';
+
+		System.out.println("The addition is: "+c);
+		System.out.println("The character is: "+f);
+		System.out.println("Both of combined is: "+c+f);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println("Some space: "+c+" "+f);
+	}
+}
